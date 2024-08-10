ce3dd55f99e9d17b1ac31b474bba370daa85ce2a
splach-coder <anasbenabbou776@gmail.com>
2024-08-10 18:36:01 +0100

    finish the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import logo from "../../assets/images/logo.webp";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,54 +10,85 @@ const Header = () => {
     setIsOpen(!isOpen);
   };
 
+  const imageSize = isScrolled ? "w-[75px] h-[75px]" : "w-[150px] h-[150px]";
+
+  const NavIcons = () => {
+    return (
+      <div className="flex">
+        <button aria-label="Search" className="text-xl">
+          <span>Loop</span>
+        </button>
+        <button aria-label="Cart" className="text-xl">
+          <span>Shopping cart</span>
+        </button>
+      </div>
+    );
+  };
+
   useEffect(() => {
     const handleScroll = () => {
       setIsScrolled(window.scrollY > 50);
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
   return (
-    <header className={`subheader ${isScrolled ? 'fixed top-0 left-0 right-0 bg-white/60 backdrop-blur-sm shadow-m' : ''} w-full bg-white shadow-md transition-all`}>
+    <header
+      className={`subheader ${
+        isScrolled
+          ? "fixed top-0 left-0 right-0 bg-white/60 backdrop-blur-lg shadow-m z-50"
+          : ""
+      } w-full bg-white shadow-md transition-all duration-350`}>
       {/* desktop design */}
-      <div className="container flex flex-col items-center p-4" >
+      <div
+        className={`container flex items-center py-4 px-12 ${
+          isScrolled ? " justify-between" : " flex-col "
+        }`}>
 
-        <div className="flex flex-col items-center py-5">
-          <img src="/path-to-logo/logo.png" alt="Logo" className="h-10 mr-4" />
-          <span className="text-xl font-bold">Meddina Artisanat</span>
+        <div className="flex flex-col items-center">
+          <img src={logo} alt="Logo" className={imageSize} />
         </div>
 
-        <div className="hidden lg:flex justify-center py-5 space-x-4">
-          <Link to="/" className="text-sm font-medium">
-            SOLDES D'ÉTÉ
-          </Link>
-          <Link to="/mixmatch" className="text-sm font-medium">
-            LE MIX&MATCH DU MOIS
-          </Link>
-          <Link to="/table" className="text-sm font-medium">
-            ARTS DE LA TABLE
-          </Link>
-          <Link to="/sets" className="text-sm font-medium">
-            NOS SETS&COFFRETS
-          </Link>
-          <Link to="/decoration" className="text-sm font-medium">
-            DÉCORATION
-          </Link>
-          <Link to="/scents" className="text-sm font-medium">
-            SENTEURS D'INTÉRIEURS
-          </Link>
-          <Link to="/lifestyle" className="text-sm font-medium">
-            LIFE STYLE
-          </Link>
-          <Link to="/infos" className="text-sm font-medium">
-            INFOS
-          </Link>
+        <div className="hidden lg:w-full lg:flex justify-center py-1">
+          <div className="space-x-4">
+            <Link to="/" className="text-sm font-medium">
+              SOLDES D'ÉTÉ
+            </Link>
+            <Link to="/mixmatch" className="text-sm font-medium">
+              LE MIX&MATCH DU MOIS
+            </Link>
+            <Link to="/table" className="text-sm font-medium">
+              ARTS DE LA TABLE
+            </Link>
+            <Link to="/sets" className="text-sm font-medium">
+              NOS SETS&COFFRETS
+            </Link>
+            <Link to="/decoration" className="text-sm font-medium">
+              DÉCORATION
+            </Link>
+            <Link to="/scents" className="text-sm font-medium">
+              SENTEURS D'INTÉRIEURS
+            </Link>
+            <Link to="/lifestyle" className="text-sm font-medium">
+              LIFE STYLE
+            </Link>
+            <Link to="/infos" className="text-sm font-medium">
+              INFOS
+            </Link>
+          </div>
+
+          <div className="absolute right-0">
+            {!isScrolled && <NavIcons />}
+          </div>
         </div>
 
+        {isScrolled && <NavIcons />}
+
+        {/* burger menu */}
         <div className="lg:hidden flex items-center">
           <button
             aria-label="Toggle Menu"
@@ -66,7 +98,6 @@ const Header = () => {
           </button>
         </div>
       </div>
-
 
       {/* phone design */}
       {isOpen && (
